f2a73242bfd230238c892b9e53433e2495c6d29e
yarob <yarob.hm@gmail.com>
2018-03-30 16:26:22 +0300

    adding f6 and f7 and class Class2

---
+++ Practice/Class2.swift
@@ -0,0 +1,16 @@
+//
+//  Class2.swift
+//  Practice
+//
+//  Created by يعرب المصطفى on 3/30/18.
+//  Copyright © 2018 yarob. All rights reserved.
+//
+
+import Foundation
+class Class2
+{
+    init()
+    {
+        print("not hello world :)")
+    }
+}
