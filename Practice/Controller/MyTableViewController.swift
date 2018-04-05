ea0034d7b017baaa8a4b99534f38aaccd6e88a4a
Ammar AlTahhan <amr.tahhan@gmail.com>
2018-04-05 14:24:24 +0300

    Change uitableviewcell background and selection color

--- Practice/Controller/MyTableViewController.swift
+++ Practice/Controller/MyTableViewController.swift
@@ -65,6 +65,11 @@ class MyTableViewController: UITableViewController {
         cell.setupColor(index: indexPath.row)
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
  
 
     /*
